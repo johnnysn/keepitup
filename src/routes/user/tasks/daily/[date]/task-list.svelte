83cfdd729cc815e65e3611e7042ef6d85f84ad9a
johnnysn <johnnysn@gmail.com>
2025-04-22 15:10:17 -0300

    feat: Show message when there is no task for the day

--- src/routes/user/tasks/daily/[date]/task-list.svelte
+++ src/routes/user/tasks/daily/[date]/task-list.svelte
@@ -28,15 +28,19 @@
 	onfinalize={handleDndFinalize}
 	class="flex w-full flex-col items-center gap-1.5 pb-2 pt-4"
 >
-	{#each items as item (item.id)}
-		<li
-			animate:flip={{ duration: flipDurationMs }}
-			class="flex w-full max-w-screen-sm items-center rounded border border-transparent bg-background/40 hover:border-border"
-		>
-			<div use:dragHandle class="mx-1 text-foreground/50 hover:text-foreground">
-				<GripVertical class="size-6" />
-			</div>
-			<TaskItem task={item} />
-		</li>
-	{/each}
+	{#if items.length > 0}
+		{#each items as item (item.id)}
+			<li
+				animate:flip={{ duration: flipDurationMs }}
+				class="flex w-full max-w-screen-sm items-center rounded border border-transparent bg-background/40 hover:border-border"
+			>
+				<div use:dragHandle class="mx-1 text-foreground/50 hover:text-foreground">
+					<GripVertical class="size-6" />
+				</div>
+				<TaskItem task={item} />
+			</li>
+		{/each}
+	{:else}
+		No tasks for today 😀
+	{/if}
 </ul>
