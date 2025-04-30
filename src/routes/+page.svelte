d2fb797b7bd00caca4a7bff64c96a7032104a921
johnnysn <johnnysn@gmail.com>
2025-04-30 11:20:05 -0300

    feat: Add hero section to the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,15 @@
 <script lang="ts">
 </script>
 
-Welcome
+<div class="mt-6 flex w-full flex-col items-center px-2">
+	<h1 class="mb-2">
+		<img src="/hero-mid.png" alt="Keep It Up Logo" class="dark:hidden md:w-[600px]" />
+		<img src="/hero-mid-dark.png" alt="Keep It Up Logo" class="hidden dark:block md:w-[600px]" />
+	</h1>
+
+	<h2 class="text-center text-3xl font-bold md:text-7xl">
+		Face your <span class="text-seco">daily</span> tasks <br /> like a
+		<span class="text-prim">true</span>
+		knight!
+	</h2>
+</div>
