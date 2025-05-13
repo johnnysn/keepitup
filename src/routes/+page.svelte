8c5d84476daf1494a18bd49b70c8e281d592bde8
johnnysn <johnnysn@gmail.com>
2025-05-13 11:57:09 -0300

    feat: Change hero image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,12 @@
 
 <div class="mt-6 flex w-full flex-col items-center px-2">
 	<h1 class="mb-2">
-		<img src="/hero-mid.png" alt="Keep It Up Logo" class="dark:hidden md:w-[600px]" />
-		<img src="/hero-mid-dark.png" alt="Keep It Up Logo" class="hidden dark:block md:w-[600px]" />
+		<img src="/hero.png" alt="Face your daily tasks like a true knight!" class="md:w-[600px]" />
 	</h1>
 
-	<h2 class="text-center text-3xl font-bold md:text-7xl">
+	<!-- <h2 class="text-center text-3xl font-bold md:text-7xl">
 		Face your <span class="text-seco">daily</span> tasks <br /> like a
 		<span class="text-prim">true</span>
 		knight!
-	</h2>
+	</h2> -->
 </div>
