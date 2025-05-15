7c3e3c5783f3eeb526b6c423453cca51a36a9590
johnnysn <johnnysn@gmail.com>
2025-05-15 11:00:23 -0300

    feat: Improve landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,50 @@
 <script lang="ts">
 </script>
 
-<div class="mt-6 flex w-full flex-col items-center px-2">
-	<h1 class="mb-2">
-		<img src="/hero.png" alt="Face your daily tasks like a true knight!" class="md:w-[600px]" />
-	</h1>
+<div class="mt-2 flex w-full flex-col items-center px-2 md:gap-8">
+	<section class="mb-2 md:mb-6">
+		<h1>
+			<img src="/hero.png" alt="Face your daily tasks like a true knight!" class="md:w-[600px]" />
+		</h1>
+	</section>
 
-	<!-- <h2 class="text-center text-3xl font-bold md:text-7xl">
-		Face your <span class="text-seco">daily</span> tasks <br /> like a
-		<span class="text-prim">true</span>
-		knight!
-	</h2> -->
+	<section class="mb-4 flex w-full flex-col items-center">
+		<h2 class="mb-4 text-center text-lg font-bold md:text-xl">
+			Here is what <span class="text-prim">Keep It Up</span> can do for you
+		</h2>
+		<div class="mb-4 flex w-full flex-wrap justify-center gap-4 text-sm md:text-base">
+			<div class="flex max-w-[270px] flex-col items-center">
+				<h2 class="mb-2 font-semibold">🗓️ Effortless Daily Planning</h2>
+
+				<p class="text-center">
+					Quickly add and manage your daily tasks with a clean, distraction-free interface —
+					designed to help you stay on track without overwhelm.
+				</p>
+			</div>
+
+			<div class="flex max-w-[270px] flex-col items-center">
+				<h2 class="mb-2 font-semibold">🔁 Smart Recurring Tasks</h2>
+
+				<p class="text-center">
+					Set tasks to repeat on specific weekdays. Perfect for routines, habits, and remembering
+					the little things that matter most.
+				</p>
+			</div>
+
+			<div class="flex max-w-[270px] flex-col items-center">
+				<h2 class="mb-2 font-semibold">🧠 Built with ADHD in Mind</h2>
+
+				<p class="text-center">
+					Gentle prompts, simple flows, and zero clutter. Finally, a task manager that understands
+					how your brain works.
+				</p>
+			</div>
+		</div>
+	</section>
+	<button
+		type="button"
+		class="rounded border border-prim bg-transparent px-6 py-4 text-xl font-semibold text-prim transition delay-100 duration-300 hover:border-background hover:bg-foreground hover:text-background"
+	>
+		Start tackling tasks now!
+	</button>
 </div>
